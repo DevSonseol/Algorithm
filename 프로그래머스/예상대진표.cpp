9945b8b35f62e5da6e94257eba5b9fcdb4f4785f
DevSonseol <seol722@yi.ac.kr>
2022-11-11 09:44:22 +0900

    Create 예상대진표.cpp

---
+++ 프로그래머스/예상대진표.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int solution(int n, int a, int b)
+{
+	int answer = 0;
+
+	while (a != b)
+	{
+		int aRemainer = a / 2;
+		int aShare = a % 2;
+
+		int bRemainer = b / 2;
+		int bShare = b % 2;
+
+		a = aRemainer + aShare;
+		b = bRemainer + bShare;
+
+		answer++;
+	}
+
+	return answer;
+}
