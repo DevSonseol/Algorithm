7e0feb4aca5c2c1848209c204fabcec8acf29edc
DevSonseol <seol722@yi.ac.kr>
2022-11-19 09:51:19 +0900

    Create 백준1911_흙길보수.cpp

---
+++ 백준/백준1911_흙길보수.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+
+    int N;
+    int L;
+
+    cin >> N >> L;
+
+    vector<pair<int, int>> v;
+
+    for (int i = 0; i < N; i++)
+    {
+        int start, end;
+        cin >> start >> end;
+
+        v.push_back({ start,end });
+    }
+
+    sort(v.begin(), v.end());
+
+    int result = 0;
+    int idx = 0;
+
+    for (int i = 0; i < N; i++)
+    {
+        if (idx >= v[i].second)
+        {
+            continue;
+        }
+
+        idx = max(idx, v[i].first);
+
+        int cnt = (v[i].second - (idx + 1)) / L + 1;
+        result += cnt;
+        idx += L * cnt;
+    }
+
+    cout << result << "\n";
+    return 0;
+}
