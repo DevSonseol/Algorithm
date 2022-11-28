0a0ae61b3a05baaa402515d150212c9938cecddf
DevSonseol <seol722@yi.ac.kr>
2022-11-29 08:43:25 +0900

    Create 숫자게임.cpp

---
+++ 프로그래머스/숫자게임.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> A, vector<int> B) {
+    int answer = 0;
+
+    sort(A.begin(), A.end(), greater());
+    sort(B.begin(), B.end(), greater());
+
+    for (int i = 0; i < A.size(); i++)
+        if (A[i] < B[answer])
+            answer++;
+
+    return answer;
+}
