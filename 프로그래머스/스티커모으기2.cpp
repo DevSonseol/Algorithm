cbaf3923596738f287cfca20315ed82738eca5c5
DevSonseol <seol722@yi.ac.kr>
2022-12-09 09:00:05 +0900

    Create 스티커모으기2.cpp

---
+++ 프로그래머스/스티커모으기2.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int dp[100001];
+
+int solution(vector<int> sticker)
+{
+    int answer = 0;
+    int n = sticker.size();
+
+    if (n == 1)
+        return sticker[0];
+
+    //Ã¹¹øÂ°²¨ ¶âÀº°æ¿ì
+    dp[0] = sticker[0];
+    dp[1] = sticker[0];
+
+    for (int i = 2; i < n - 1; i++)
+    {
+        dp[i] = max(dp[i - 2] + sticker[i], dp[i - 1]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (answer < dp[i])
+            answer = dp[i];
+    }
+
+    //µÎ¹øÂ°²¨ ¶âÀº°æ¿ì
+    dp[0] = 0;
+    dp[1] = sticker[1];
+
+    for (int i = 2; i < n; i++)
+    {
+        dp[i] = max(dp[i - 2] + sticker[i], dp[i - 1]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (answer < dp[i])
+            answer = dp[i];
+    }
+
+    return answer;
+}
